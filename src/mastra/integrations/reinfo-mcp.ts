098b7f78d4c82054fb50e7ce9ace6021483a044c
k1mu21 <jbytq@yahoo.co.jp>
2026-04-07 00:00:01 +0900

    MCP変更

--- src/mastra/integrations/reinfo-mcp.ts
+++ src/mastra/integrations/reinfo-mcp.ts
@@ -3,7 +3,7 @@ import { MCPClient } from '@mastra/mcp';
 export const reinfoMcp = new MCPClient({
   servers: {
     reinfo: {
-      url: new URL('https://mcp.n-3.ai/mcp?tools=get-time,reinfolib-real-estate-price,reinfolib-city-list'),
+      url: new URL('https://mcp.n-3.ai/mcp?tools=rakuten-travel-hotel-search'),
     },
   },
 });
