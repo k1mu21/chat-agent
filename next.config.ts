89ef4ef3b3ae56e15693e8dd5a9c97ebafae95cd
k1mu21 <jbytq@yahoo.co.jp>
2026-04-13 21:08:29 +0900

    Revert "serverExternalPackagesの設定を削除"
    This reverts commit e577ee2ece683e4936a71c66e38a94e2f235d0a2.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["@mastra/*"],
+};
 
 export default nextConfig;
 
